71764896b5548395137b4c3decb6ccbae1283ef6
Dvwhite81 <Dvwhite1981@gmail.com>
2024-01-12 08:57:20 -0500

    complete

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 // Your code here
+const Employee = require("./employee");
+
+const johnWick = new Employee('John Wick', 'Dog Lover');
+
+// setTimeout(johnWick.sayName, 2000);
+
+const johnSayName = johnWick.sayName.bind(johnWick);
+setTimeout(johnSayName, 2000);
+
+// setTimeout(johnWick.sayOccupation, 3000);
+
+const johnSayJob = johnWick.sayOccupation.bind(johnWick);
+setTimeout(johnSayJob, 3000);
